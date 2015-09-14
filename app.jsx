050f1fd07f098da1de193906399d94107c9149c6
Ismael Slimane <isma.bled777@gmail.com>
2015-09-14 18:31:21 +0200

    Added click event on a button that shows a panel on another div

--- app.jsx
+++ app.jsx
@@ -13,28 +13,43 @@ const panelInstance = (
   );
 
 function handleClick() {
-  React.render(panelInstance, demo);
+  React.render(panelInstance, panels);
 }
 
-const buttonGroupInstance = (
-  <ButtonGroup vertical>
-    <Button onClick={handleClick}>Create div</Button>
-    <Button>Button</Button>
-    <DropdownButton title="Dropdown" id="bg-vertical-dropdown-1">
-      <MenuItem eventKey="1">Dropdown link</MenuItem>
-      <MenuItem eventKey="2">Dropdown link</MenuItem>
-    </DropdownButton>
-    <Button>Button</Button>
-    <Button>Button</Button>
-    <DropdownButton title="Dropdown" id="bg-vertical-dropdown-2">
-      <MenuItem eventKey="1">Dropdown link</MenuItem>
-      <MenuItem eventKey="2">Dropdown link</MenuItem>
-    </DropdownButton>
-    <DropdownButton title="Dropdown" id="bg-vertical-dropdown-3">
-      <MenuItem eventKey="1">Dropdown link</MenuItem>
-      <MenuItem eventKey="2">Dropdown link</MenuItem>
-    </DropdownButton>
-  </ButtonGroup>
-);
+var ButtonGroupInstance = React.createClass({
+  render: function(){
+    return(
+      <ButtonGroup vertical>
+        <Button onClick={handleClick}>Create div</Button>
+        <Button>Button</Button>
+        <DropdownButton title="Dropdown" id="bg-vertical-dropdown-1">
+          <MenuItem eventKey="1">Dropdown link</MenuItem>
+          <MenuItem eventKey="2">Dropdown link</MenuItem>
+        </DropdownButton>
+        <Button>Button</Button>
+        <Button>Button</Button>
+        <DropdownButton title="Dropdown" id="bg-vertical-dropdown-2">
+          <MenuItem eventKey="1">Dropdown link</MenuItem>
+          <MenuItem eventKey="2">Dropdown link</MenuItem>
+        </DropdownButton>
+        <DropdownButton title="Dropdown" id="bg-vertical-dropdown-3">
+          <MenuItem eventKey="1">Dropdown link</MenuItem>
+          <MenuItem eventKey="2">Dropdown link</MenuItem>
+        </DropdownButton>
+      </ButtonGroup>
+    );
+  }
+});
 
-React.render(buttonGroupInstance, demo);
+var Page = React.createClass({
+  render: function(){
+    return(
+      <div>
+        <ButtonGroupInstance />
+        <div id="panels"></div>
+      </div>
+    );
+  }
+});
+
+React.render(<Page />, demo);
