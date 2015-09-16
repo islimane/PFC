d09d88be740eebf26c3bf532dea825221b5e03be
Ismael Slimane <isma.bled777@gmail.com>
2015-09-16 22:30:08 +0200

    Fetching JSON data combined with react-bootstrap 'sidebar'

--- app.jsx
+++ app.jsx
@@ -5,22 +5,42 @@ var DropdownButton = ReactBootstrap.DropdownButton;
 var MenuItem = ReactBootstrap.MenuItem;
 var Panel = ReactBootstrap.Panel;
 
+var PanelInstance = React.createClass({
+  render: function(){
+    return(
+      <Panel>
+        <h3>Author: {this.props.author}</h3>
+        {this.props.text}
+      </Panel>
+    );
+  }
+});
 
-const panelInstance = (
-    <Panel>
-      Basic panel example
-    </Panel>
-  );
-
-function handleClick() {
-  React.render(panelInstance, panels);
-}
+var PanelSet = React.createClass({
+  render: function(){
+    var panels = this.props.state;
+    if(this.props.state!=="")
+      panels = this.props.state.data.map(function (panel) {
+        return (
+          <PanelInstance author={panel.author} text={panel.text} />
+        );
+      });
+    return(
+      <div id="panels">
+        {panels}
+      </div>
+    );
+  }
+});
 
 var ButtonGroupInstance = React.createClass({
+  handleClick: function() {
+    React.render(<PanelSet state={this.props.state}/>, document.getElementById('panels'));
+  },
   render: function(){
     return(
       <ButtonGroup vertical>
-        <Button onClick={handleClick}>Create div</Button>
+        <Button onClick={this.handleClick}>Create div</Button>
         <Button>Button</Button>
         <DropdownButton title="Dropdown" id="bg-vertical-dropdown-1">
           <MenuItem eventKey="1">Dropdown link</MenuItem>
@@ -42,17 +62,32 @@ var ButtonGroupInstance = React.createClass({
 });
 
 var Page = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
   render: function(){
     return(
       <div>
         <div id="options">
-          <ButtonGroupInstance />
+          <ButtonGroupInstance state={this.state}/>
         </div>
-        <div id="panels"></div>
+        <PanelSet state=""/>
       </div>
     );
   }
 });
 
-
-React.render(<Page />, demo);
+React.render(<Page url="data.json"/>, demo);
