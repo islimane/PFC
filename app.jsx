56d4ce226d9a40cf7e11ae139e422cc92c36688f
Ismael Slimane Zubillaga <isma.bled777@gmail.com>
2016-02-06 13:46:30 +0100

    Updated app.jsx file to master branch state

--- app.jsx
+++ app.jsx
@@ -9,18 +9,7 @@ var DropdownButton = ReactBootstrap.DropdownButton;
 var MenuItem = ReactBootstrap.MenuItem;
 var Panel = ReactBootstrap.Panel;
 
-var PanelInstance = React.createClass({
-  render: function(){
-    return(
-      <Panel>
-        <h3>Author: {this.props.author}</h3>
-        {this.props.text}
-      </Panel>
-    );
-  }
-});
 
-<<<<<<< HEAD
 
 /////////
 // App //
@@ -44,17 +33,6 @@ var PanelSet = React.createClass({
                       <PanelInstance author={panel.author} text={panel.text} />
                     );
                 });
-=======
-var PanelSet = React.createClass({
-  render: function(){
-    var panels = this.props.state;
-    if(this.props.state!=="")
-      panels = this.props.state.data.map(function (panel) {
-        return (
-          <PanelInstance author={panel.author} text={panel.text} />
-        );
-      });
->>>>>>> 550c50ba922c83aae3287041080001a18b6d5f68
     return(
       <div id="panels">
         {panels}
@@ -65,20 +43,12 @@ var PanelSet = React.createClass({
 
 var ButtonGroupInstance = React.createClass({
   handleClick: function() {
-<<<<<<< HEAD
     this.props.onShowCommentsClick();
-=======
-    React.render(<PanelSet state={this.props.state}/>, document.getElementById('panels'));
->>>>>>> 550c50ba922c83aae3287041080001a18b6d5f68
   },
   render: function(){
     return(
       <ButtonGroup vertical>
-<<<<<<< HEAD
         <Button onClick={this.handleClick}>Show Comments</Button>
-=======
-        <Button onClick={this.handleClick}>Create div</Button>
->>>>>>> 550c50ba922c83aae3287041080001a18b6d5f68
         <Button>Button</Button>
         <DropdownButton title="Dropdown" id="bg-vertical-dropdown-1">
           <MenuItem eventKey="1">Dropdown link</MenuItem>
@@ -105,11 +75,7 @@ var Page = React.createClass({
   getInitialState: function() {
     return {data: []};
   },
-<<<<<<< HEAD
   loadCommentsFromServer: function() {
-=======
-  componentDidMount: function() {
->>>>>>> 550c50ba922c83aae3287041080001a18b6d5f68
     $.ajax({
       url: this.props.url,
       dataType: 'json',
@@ -122,26 +88,17 @@ var Page = React.createClass({
       }.bind(this)
     });
   },
-<<<<<<< HEAD
   handleShowComments: function() {
     this.loadCommentsFromServer();
   },
-=======
->>>>>>> 550c50ba922c83aae3287041080001a18b6d5f68
   render: function(){
     return(
       <div>
         <div id="options">
-<<<<<<< HEAD
           <ButtonGroupInstance
           onShowCommentsClick={this.handleShowComments}/>
         </div>
         <PanelSet data={this.state.data}/>
-=======
-          <ButtonGroupInstance state={this.state}/>
-        </div>
-        <PanelSet state=""/>
->>>>>>> 550c50ba922c83aae3287041080001a18b6d5f68
       </div>
     );
   }
